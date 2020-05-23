58219911e523b3d8b83f9d8e210e206b7a55566e
layefly <64159110+layefly@users.noreply.github.com>
2020-05-23 07:53:01 -0500

    kk

--- src/app/licenciaturas/actuaria/actuaria.page.ts
+++ src/app/licenciaturas/actuaria/actuaria.page.ts
@@ -8,7 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ActuariaPage implements OnInit {
 
   constructor() { }
-
+  secciones = [
+    {
+      titulo:"Objetivo:",
+      icon: "play-outline",
+      texto:"Formar Licenciados en Actuaría apegados a los lineamientos educativos internacionales del gremio "+
+       "actuarial en base a los Modelos Educativo y Académico de la UANL."
+    },
+    {
+      titulo:"Perfil del Egresado:",
+      icon: "play-outline",
+      texto:"Formar Licenciados en Actuaría con un perfil integral, altamente competentes en el mundo laboral, capaces de entender holísticamente la realidad,"+
+      " con plena conciencia de la situación regional, nacional y mundial, comprometidos con el desarrollo sustentable, económico, tecnológico y "+
+      "social de la humanidad capaces de estudiar, plantear y formular modelos de contenido matemático así como estadístico y probabilístico,"+
+      " información para la planeación, la previsión y la toma de decisiones acerca de fenómenos que involucran riesgos, además de ofrecer con el fin de proveer"+
+      " soluciones relevantes y asesoría experta a problemas financieros, empresariales y sociales que involucren eventos futuros inciertos, tanto en el ámbito laboral público como privado."
+    }
+  ]
   ngOnInit() {
   }
 
